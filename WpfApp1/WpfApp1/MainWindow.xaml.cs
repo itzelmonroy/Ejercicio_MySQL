5152530eed0405080e994e3f4e32ed1f3ee67ad7
Alumno <Alumno@PC10>
2019-11-05 10:19:02 -0600

    Para mañana

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Clases;//Agregamos dos librerias 
+using SQLite; 
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Class1 contacto = new Class1()
+            {
+                Nombre = txtNombre.Text,
+                Correo = txtCorreo.Text,
+                Telefono = txtTelefono.Text,
+            };
+            using(SQLiteConnection conexion = new SQLiteConnection(App.databasePath))
+            {
+                conexion.CreateTable<Class1>();
+                conexion.Insert(contacto);
+            }
+            
+
+             
+            
+
+        }
+    }
+}
